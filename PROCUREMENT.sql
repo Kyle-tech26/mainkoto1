fe00ded2f0acfe55f64ba73ddc172f1b940b230b
Kyle-tech26 <kyliejavier26@gmail.com>
2025-03-18 10:55:10 -0700

    Add files via upload

--- PROCUREMENT.sql
+++ PROCUREMENT.sql
@@ -9,3 +9,17 @@ CREATE TABLE `supplies_request_form` (
   `supplier_company` varchar(100) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci
+
+
+
+CREATE TABLE `items` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `popup_name` varchar(255) DEFAULT NULL,
+  `popup_image` varchar(255) DEFAULT NULL,
+  `available_products` varchar(255) DEFAULT NULL,
+  `option_name` varchar(255) DEFAULT NULL,
+  `quantity` int(11) DEFAULT NULL,
+  `size` varchar(50) DEFAULT NULL,
+  `color` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=52 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci
